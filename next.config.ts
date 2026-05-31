69024c4621cf16dfd7002a59167ec51a34603f73
TruongCongTri <congtritruong1994@gmail.com>
2026-06-01 02:35:24 +0700

    handle compiler from choking on ArcGIS

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   output: "standalone",
+  swcMinify: false,
 };
 
 export default nextConfig;
