544c58d53fd74b9f257acce132ff84afab0ca764
TruongCongTri <congtritruong1994@gmail.com>
2026-06-01 01:42:05 +0700

    optimize build time

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -14,6 +14,7 @@ import type {
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1",
   withCredentials: true,
+  timeout: 10000,
 });
 
 //
